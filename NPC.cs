4eb44960322bf66056f91d4b089070f7256b5f1e
Benjamin Russell <40721667+russellpuff@users.noreply.github.com>
2022-07-22 07:43:01 -0400

    Fix git screwup

---
+++ NPC.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CofD_NPC
+{
+    [Serializable]
+    [XmlRoot("NPCRoot")]
+    public class NPC
+    {
+        [XmlAttribute("ID")]
+        public long ID { get; set; }
+
+        [XmlAttribute("Name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("Age")]
+        public int Age { get; set; }
+
+        [XmlAttribute("Virtue")]
+        public string Virtue { get; set; }
+
+        [XmlAttribute("Vice")]
+        public string Vice { get; set; }
+
+        [XmlAttribute("Concept")]
+        public string Concept { get; set; }
+
+        [XmlAttribute("HealthDots")]
+        public int HealthDots { get; set; }
+
+        [XmlAttribute("HealthStates")]
+        public int[] HealthStates { get; set; }
+
+        [XmlAttribute("WillpowerDots")]
+        public int WillpowerDots { get; set; }
+
+        [XmlAttribute("WillpowerCurrent")]
+        public int WillpowerCurrent { get; set; }
+
+        [XmlAttribute("Integrity")]
+        public int Integrity { get; set; }
+
+        [XmlAttribute("Size")]
+        public int Size { get; set; }
+
+        [XmlAttribute("Speed")]
+        public int Speed { get; set; }
+
+        [XmlAttribute("Defense")]
+        public int Defense { get; set; }
+
+        [XmlAttribute("Armor")]
+        public string Armor { get; set; }
+
+        [XmlAttribute("Initiative")]
+        public int Initiative { get; set; }
+
+        [XmlAttribute("Description")]
+        public string Description { get; set; }
+
+        [XmlAttribute("Intelligence")]
+        public int Intelligence { get; set; }
+
+        [XmlAttribute("Wits")]
+        public int Wits { get; set; }
+
+        [XmlAttribute("Resolve")]
+        public int Resolve { get; set; }
+
+        [XmlAttribute("Strength")]
+        public int Strength { get; set; }
+
+        [XmlAttribute("Dexterity")]
+        public int Dexterity { get; set; }
+
+        [XmlAttribute("Stamina")]
+        public int Stamina { get; set; }
+
+        [XmlAttribute("Presence")]
+        public int Presence { get; set; }
+
+        [XmlAttribute("Manipulation")]
+        public int Manipulation { get; set; }
+
+        [XmlAttribute("Composure")]
+        public int Composure { get; set; }
+
+        [XmlAttribute("Skills")]
+        public string[] Skills { get; set; }
+
+        [XmlAttribute("SkillDots")]
+        public int[] SkillDots { get; set; }
+
+        [XmlAttribute("Merits")]
+        public string[] Merits { get; set; }
+
+        [XmlAttribute("MeritDots")]
+        public int[] MeritDots { get; set; }
+
+        [XmlAttribute("Conditions")]
+        public string Conditions { get; set; }
+
+        [XmlAttribute("Aspirations")]
+        public string Aspirations { get; set; }
+
+        public NPC()
+        {
+            ID = DateTime.Now.Ticks;
+            Name = "";
+            Age = 0;
+            Virtue = "";
+            Vice = "";
+            Concept = "";
+            HealthDots = 0;
+            HealthStates = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            WillpowerDots = 0;
+            WillpowerCurrent = 0;
+            Integrity = 0;
+            Size = 0;
+            Speed = 0;
+            Defense = 0;
+            Armor = "";
+            Initiative = 0;
+            Description = "";
+            Intelligence = 0;
+            Wits = 0;
+            Resolve = 0;
+            Strength = 0;
+            Dexterity = 0;
+            Stamina = 0;
+            Presence = 0;
+            Manipulation = 0;
+            Composure = 0;
+            Skills = new string[16] { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+            SkillDots = new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Merits = new string[10] { "", "", "", "", "", "", "", "", "", "" };
+            MeritDots = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Conditions = "";
+            Aspirations = ""; 
+        }
+    
+    }
+
+    public class DataItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ID { get; set; }
+    }
+}
---
+++ SheetWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO.Compression;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace CofD_NPC
+{
+    /// <summary>
+    /// Interaction logic for Sheet2.xaml
+    /// </summary>
+    public partial class SheetWindow : Window
+    {
+#nullable disable
+        BitmapImage healthy, bashing, lethal, aggriv;
+#nullable enable
+        readonly Dictionary<string, int> skillboxes = new();
+        List<DataItem> dataGridItems = new();
+        List<NPC> NPCs = new();
+        NPC SFNPC;
+
+
+        public SheetWindow()
+        {
+            InitializeComponent();
+            InitializeImages();
+            LoadSBDictionary();
+            LoadNPCList();
+            sw10AgainRadio.IsChecked = true;
+        }
+
+        private void InitializeImages()
+        {
+            healthy = new BitmapImage(new Uri("pack://application:,,,/Resources/healthy_transparent.png"));
+            bashing = new BitmapImage(new Uri("pack://application:,,,/Resources/bashing_yellow.png"));
+            lethal = new BitmapImage(new Uri("pack://application:,,,/Resources/lethal_yellow.png"));
+            aggriv = new BitmapImage(new Uri("pack://application:,,,/Resources/aggrivated_yellow.png"));
+            foreach (UIElement c in swHealthStateGrid.Children)
+            {
+                if (c.GetType() == typeof(Image))
+                {
+                    Image im = (Image)c;
+                    if (im.Name.Contains("HealthState")) { im.Source = healthy; }
+                }
+            }
+        }
+
+        private void LoadSBDictionary()
+        {
+            for (int i = 1; i <= 16; ++i) { skillboxes.Add($"swSkillComboBox{i}", 0); }
+        }
+
+        private void LoadNPCList()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/NPC/";
+            Directory.CreateDirectory(path);
+
+            DirectoryInfo di = new(path);
+            foreach (var file in di.GetFiles("*.npc"))
+            {
+                try
+                {
+                    NPC n = DecompressFile(file);
+                    NPCs.Add(n);
+                    dataGridItems.Add(new DataItem() { Name = n.Name, Description = n.Description, ID = n.ID.ToString() });
+                }
+                catch
+                {
+                    string m = "File " + file.Name + " was corrupted and could not be loaded.\n";
+                    MessageBox.Show(m, "Corrupt file", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            swDataGrid.ItemsSource = dataGridItems;
+            SetAllDataGridCellStyles();
+            swDataGrid.Items.Refresh();
+        }
+
+        private static NPC DecompressFile(FileInfo file)
+        {
+            using FileStream fs = new(file.FullName, FileMode.Open, FileAccess.Read);
+            using MemoryStream ums = new();
+            using (DeflateStream ds = new(fs, CompressionMode.Decompress)) { ds.CopyTo(ums); }
+            ums.Position = 0;
+            XmlSerializer xs = new(typeof(NPC));
+            return (NPC)xs.Deserialize(ums);
+        }
+
+        private void Main_Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) { }
+
+        private void Dots_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+            RatingBar rb = (RatingBar)sender;
+            string dotname = rb.Name;
+            bool istop = false;
+
+            if (dotname.Contains("Top"))
+            {
+                dotname = dotname.Replace("Top", "");
+                dotname += "Bottom";
+                istop = true;
+            }
+            else
+            {
+                dotname = dotname.Replace("Bottom", "");
+                dotname += "Top";
+            }
+
+            RatingBar other = (RatingBar)((Grid)rb.Parent).FindName(dotname);
+            // Bottom max switch to 9 and 10 again is to interrupt spinning animation.
+            // I can't figure out how else to disable it.
+#nullable disable
+            var cv = new BrushConverter();
+            Brush gold = (Brush)cv.ConvertFromString("#FFFFC107");
+            // Prevent valuechanged event from firing lol.
+            rb.ValueChanged -= Dots_ValueChanged;
+            other.ValueChanged -= Dots_ValueChanged;
+            if (istop)
+            {
+                rb.Max = rb.Value;
+                rb.Foreground = gold;
+                other.Max = 9;
+                other.Max = 10;
+                other.Value = 0;
+            }
+            else
+            {
+                other.Max = rb.Value;
+
+                other.Value = rb.Value;
+                other.Foreground = gold;
+                rb.Max = 9;
+                rb.Max = 10;
+                rb.Value = 0;
+            }
+            rb.ValueChanged += Dots_ValueChanged;
+            other.ValueChanged += Dots_ValueChanged;
+#nullable enable
+        }
+
+        private void SkillComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            ComboBoxItem? cbi = null;
+#nullable disable // shut up shut up shut up shut up shut
+            if (e.RemovedItems.Count > 0) { cbi = (ComboBoxItem)e.RemovedItems[0]; }
+#nullable enable
+            CBIterator(true, cb, cbi);
+            CBIterator(false);
+        }
+
+        private void CBIterator(bool firstpass, ComboBox? original = null, ComboBoxItem? prevItem = null)
+        { // If (firstpass), compare dupes. Otherwise update styles.
+            foreach (Control c in swMiddleGrid.Children)
+            {
+                if (c.GetType() == typeof(ComboBox))
+                {
+                    ComboBox other = (ComboBox)c;
+                    if (firstpass && original != null)
+                    {
+                        if (original == other) { continue; }
+                        ComboBoxItem orig = (ComboBoxItem)original.SelectedItem;
+                        ComboBoxItem oth = (ComboBoxItem)other.SelectedItem;
+
+                        if (oth != null)
+                        {
+                            if (orig.Content.Equals(oth.Content))
+                            {
+                                ++skillboxes[original.Name];
+                                ++skillboxes[other.Name];
+                            }
+                            if (prevItem != null && (String)oth.Content == (String)prevItem.Content)
+                            {
+                                --skillboxes[original.Name];
+                                --skillboxes[other.Name];
+                            }
+                        }
+                    } else
+                    {
+                        string style = (skillboxes[other.Name] > 0) ? "RedComboBox" : "DefaultComboBox";
+                        other.Style = FindResource(style) as Style; 
+                    }
+                }
+            }
+        }
+
+        private void HealthState_Click(object sender, MouseButtonEventArgs e)
+        {
+            Image im = (Image)sender;
+            if (im.Source == healthy) { im.Source = bashing; return; }
+            if (im.Source == bashing) { im.Source = lethal; return; }
+            if (im.Source == lethal) { im.Source = aggriv; return; }
+            if (im.Source == aggriv) { im.Source = healthy; return; }
+        }
+
+        private void DataGridContextMenu_Opened(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+#nullable disable
+        private void RollRectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var cv = new BrushConverter();
+            swRollSectionRectangle.Stroke = (Brush)cv.ConvertFromString("#FFFFC107");
+        }
+
+        private void RollRectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var cv = new BrushConverter();
+            swRollSectionRectangle.Stroke = (Brush)cv.ConvertFromString("#FF707070");
+        }
+
+        private void AddNPCButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RollButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetAllDataGridCellStyles()
+        {
+            /*
+            foreach (DataGridCell cell in swDataGrid.Items)
+            {
+                cell.Style = FindResource("DataGridCellNoFocus") as Style;
+            }*/
+        }
+    }
+}
